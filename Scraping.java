da5681e5fcf6471d4fdd133b405c04b327d55cdf
AnupGhosh2605 <53530169+AnupGhosh2605@users.noreply.github.com>
2019-09-08 18:05:59 +0530

    Update Scraping.java

--- Scraping.java
+++ Scraping.java
@@ -7,7 +7,7 @@ public class new_project {
 		// TODO Auto-generated method stub
 		try {
 
-		      // fetch the document over HTTP
+         // fetch the document over HTTP
 final Document document=Jsoup.connect("https://github.com/trending").get();
         // get the page title
 String title =document.title();
@@ -18,11 +18,11 @@ for(Element row: document.select("article.Box-row"))
        Elements links = row.select("a[href]");
 	   String link=links.attr("href");
 		
-	   String head=row.select("h1.h3.lh-condensed").text();
-	   String summery=row.select("p.col-9.text-gray.my-1.pr-4").text();
+	   String ripo_name=row.select("h1.h3.lh-condensed").text();
+	   String description=row.select("p.col-9.text-gray.my-1.pr-4").text();
 		 
-	   System.out.println("Headline :"+head);
-       System.out.println("Summery  :"+summery);
+	   System.out.println("Ripo_name :"+ripo_name);
+       System.out.println("Description :"+description);
        System.out.println("Link     :https://github.com/"+link);
 	   System.out.println("  ");
 }
